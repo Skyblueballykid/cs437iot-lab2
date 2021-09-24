17bc57fc8be3ceb4877011bfc2ef4f379b80f30c
Thomas Kalnik <kalnik.thomas@gmail.com>
2021-09-24 16:11:29 -0500

    remove fast API dependency because of uvicorn issue on pi

--- api/wifi_client.py
+++ api/wifi_client.py
@@ -3,7 +3,7 @@ import socket
 # For testing the raspberry pi API
 
 HOST = "192.168.0.3" # IP address of your Raspberry PI
-PORT = 65432          # The port used by the server
+PORT = 5000          # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
