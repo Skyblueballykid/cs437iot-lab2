afc38bcf493ae61e2a6ad44213b4e09e0e573da4
Charles Stolz <rtikes@protonmail.com>
2021-10-06 00:40:18 -0400

    added feature to write file and arg parsing

--- bluetooth/bluetooth_car_client.py
+++ bluetooth/bluetooth_car_client.py
@@ -2,18 +2,50 @@
 Execute on the remote virtual machine client
 """
 import bluetooth
+import argparse
+import json
 
-# Bluetooth address of the raspberry pi on the car
-host = "B8:27:EB:FD:5D:88"
-port = 1
-sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-sock.connect((host, port))
-while True:
-    text = input("Enter command for car:")
-    if text == "quit":
-        break
-    sock.send(text)
 
-    data = sock.recv(1024)
-    print("from car: ", data)
-sock.close()
+def write_file(car_telemetry: str, telemetry_file_name: str):
+    with open(telemetry_file_name, "w") as file_handle:
+        file_handle.write(json.dumps(car_telemetry))
+
+
+def main():
+    host = args.host
+    port = args.port
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    sock.connect((host, port))
+    while True:
+        text = input("Enter command for car:")
+        if text == "quit":
+            break
+        sock.send(text)
+        car_telemetry = sock.recv(1024).decode()
+        print("telemetry received from car: ", car_telemetry)
+        print("writing telemetry: ", car_telemetry)
+        write_file(car_telemetry, args.telemetry_file)
+    sock.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--host",
+        type=str,
+        required=False,
+        help="bluetooth address",
+        default="B8:27:EB:FD:5D:88",
+    )
+    parser.add_argument(
+        "--port", type=str, required=False, help="bluetooth address", default=1
+    )
+    parser.add_argument(
+        "--telemetry-file",
+        type=str,
+        required=False,
+        help="filename to write to disk",
+        default="car-telemetry.json",
+    )
+    args = parser.parse_args()
+    main()
